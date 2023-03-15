4e1622e8b982fca50f5ee8b776e74ce2b419c45e
Matthew Baer <matt.baer77@gmail.com>
2023-03-15 02:14:22 -0400

    register route functionality completed

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db
+from models import connect_db, db, User
+from forms import UserRegistrationForm, LoginForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask_feedback"
@@ -19,4 +21,42 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_get():
-    return render_template("register.html")
+    """
+    GET - Show a form to register a new user
+    POST - Handle a form to register a new user
+    """
+    # If userID in session - consider sending to different location
+
+    form = UserRegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+
+        db.session.add(new_user)
+
+        try:
+            db.session.commit()
+        
+        except IntegrityError:
+            form.username.errors.append('One or more field already taken')
+            return render_template('register.html', form=form)
+
+        session['username'] = new_user.username
+
+        flash('Welcome! Successfully Created Your Account!', "success")
+
+        return redirect('/secret')
+
+    else:
+
+        return render_template("register.html", form=form)
+
+@app.route('/secret')
+def secret():
+    return "<h1> You are at the secret route </h1>"
--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired
 
-class RegistrationForm(FlaskForm):
+class UserRegistrationForm(FlaskForm):
     """
     username
     password
@@ -12,10 +13,10 @@ class RegistrationForm(FlaskForm):
     """
 
     username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Username", validators=[InputRequired()])
-    email = EmailField("Username", validators=[InputRequired()])
-    first_name = StringField("first_name", validators=[InputRequired()])
-    last_name = StringField("last_name", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
 
 class LoginForm(FlaskForm):
 
